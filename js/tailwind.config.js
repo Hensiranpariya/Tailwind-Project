2a89647c45859f11d0f68f34718c20fd84664d21
Hensiranpariya <henyranpariya@gmail.com>
2023-03-14 15:29:58 +0530

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//     theme: {
+//       extend: {
+//         backgroundImage: {
+//           'hero-pattern': "url('../images/banner.jpg')",
+//         }
+//       }
+//     }
+//   }
+// module.exports = {
+//     theme: {
+//      extend :{
+//         fontSize: {
+//             'text-10xl' : '160px',
+//         }
+//      }
+//     }
+//   }
